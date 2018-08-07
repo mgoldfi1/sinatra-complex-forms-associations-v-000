cf235708704005cbf18a0131cbe90c0ea671eb67
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-07 14:20:06 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -20,6 +20,10 @@ class PetsController < ApplicationController
     end
     redirect to "pets/#{@pet.id}"
   end
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
@@ -31,8 +35,5 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
+  
 end
